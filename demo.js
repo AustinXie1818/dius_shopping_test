1772cb4118ace9ece89e022c584695127e50fb5e
Austin Xie <xie_weiyu@yeah.net>
2017-11-20 09:34:05 +1100

    clean logs

--- demo.js
+++ demo.js
@@ -13,20 +13,14 @@ import prompt from 'prompt';
 prompt.start();
 prompt.get(['skus'], function (err, result) {
   if (err) { return onErr(err); }
-  console.log('SKUs Scanned: ' + result.skus);
 
   const co = new CheckOut(createPricingRules());
   const skus = result.skus.split(',').map(sku => sku.trim()).filter(sku => sku);
-  console.log(`type: ${typeof skus}, skus: ${skus[1]}`);
   const skuItems = skus.map((sku) => {
     const skuItem = generateSkuItem(sku);
-    console.log(`sku: ${sku}, skuItem: ${JSON.stringify(skuItem)}`);
     return skuItem;
   });
-  console.log(`skuItesm: ${JSON.stringify(skuItems)}`);
-
   skuItems.forEach((skuItem) => co.checkOut(skuItem));
-
   console.log('Total expected: ', co.total());
 });
 
@@ -43,9 +37,6 @@ function createPricingRules() {
   ]
 }
 
-// const items = ['ipd', 'ipd', 'ipd'].map((sku) => generateSkuItem(sku));
-// console.log(`items: ${JSON.stringify(items)}`);
-
 function generateSkuItem(sku) {
   switch(sku) {
     case 'ipd':
@@ -61,6 +52,6 @@ function generateSkuItem(sku) {
      return new SkuItem(SKU_IDS.vga, 'VGA adapter', 3000);
 
     default:
-     return new Error(`invalid sku '${sku}'`);
+     throw new Error(`invalid sku '${sku}'`);
   }
 }
